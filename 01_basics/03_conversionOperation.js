3e457058f4208f75cf0ddf8d0fb0aab299beba26
Abdul Wahab <wahab.suit997@gmail.com>
2025-03-05 20:45:31 +0000

    converson of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "Abdul"
+
+/*
+console.log(typeof score);
+console.log(typeof (score))
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber )
+console.log(valueInNumber);
+*/
+
+
+// "33" => 33
+// "33abc" => NaN (Not A Number)
+// "true" => 1; false => 0;
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log(booleanIsLoggedIn);  
+
+/*
+1 => true ; 0 => false;
+"" => false
+"abdul" => true
+*/
+
+let Anumber = 23
+let StringNumber = String (Anumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
