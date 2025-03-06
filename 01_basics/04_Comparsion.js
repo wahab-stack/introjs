a1de89ba977f2fb8d7415e798c9e6f61bff09bb1
Abdul Wahab <wahab.suit997@gmail.com>
2025-03-06 17:56:09 +0000

    comparsion done abou null and undefined info

---
+++ 01_basics/04_Comparsion.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("2" < 1);
+
+
+//--- Aviod these type of comparsion and make the code clean and readable
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);          
+
+//--- Aviod these type of comparsion and make the code clean and readable
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+//  === (triple eqaul)
+console.log("2" === 2 );
