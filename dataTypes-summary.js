35d39c66dc6392ed0906924970b37b3be358da09
Abdul Wahab <wahab.suit997@gmail.com>
2025-03-06 18:24:44 +0000

    summary related

---
+++ dataTypes-summary.js
@@ -0,0 +1,46 @@
+// questions must ask in interveiws
+
+//  Primitive
+
+//  7 types : 
+// String----------"custome message"
+//  Number------1234 
+// Boolearn------true & false
+//  null-------empty or nothing 
+// undefined----- what value declearation
+// Symbol ------to make value unique espacially in avdvance js 
+// BigInt-----scientific values
+
+const score = 100         //number
+const scoreValue = 100.3  //number
+
+const isLoggedIn = false   //Boolean type
+const outsideTemp = null   //empty temperature for e-g '0'
+let userEmail;             //undefined value in terminal
+
+const id = Symbol('123')   //(unique ID)usage in front-end libraries
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference or (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+//  https://262.ecma-international.org/5.1/#sec-11.4.3
