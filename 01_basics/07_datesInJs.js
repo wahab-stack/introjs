e6f4fde99bdb80b29ddd834b0aff3b50b507687b
Abdul Wahab <wahab.suit997@gmail.com>
2025-03-07 07:59:08 +0000

    Date syntax

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,16 @@
+//Dates
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+
+// let myCreatedDate = new Date(2025, 2, 7)
+// let myCreatedDate = new Date(2025, 2, 7, 5, 5)
+let myCreatedDate = new Date("2025-03-07")
+console.log(myCreatedDate);
+console.log(myCreatedDate.toDateString());
+console.log(myCreatedDate.toLocaleString());
