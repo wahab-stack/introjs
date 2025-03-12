bb25cb43bd147d1170eca851337604a71b65803e
Abdul Wahab <wahab.suit997@gmail.com>
2025-03-12 09:54:26 +0000

    arrow and this

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -8,17 +8,17 @@ const user = {
     
     welcomeMessage : function () {
         console.log(`${this.username} , welcome to website`)   // this keyword refer the current context in this USER object
-        console.log(this);
+        // console.log(this);
         
     }
 }
 
-user.welcomeMessage()    // output : values of te current context 
+// user.welcomeMessage()    // output : values of te current context 
 
-user.username = "sami"   // output : sami welocme to website  (Context changed)
-user.welcomeMessage()
+// user.username = "sami"   // output : sami welocme to website  (Context changed)
+// user.welcomeMessage()
 
-console.log(this);
+// console.log(this);
 
 /*
 abdul , welcome to website
@@ -34,6 +34,79 @@ sami , welcome to website
   welcomeMessage: [Function: welcomeMessage]
 }
 */
+
+
+//++++++++++++ Arrow function ++++++++++++
+
+// function chai(){
+    
+//     console.log(this);
+    
+// }
+// chai()
+
+
+// function chai(){
+//     let username = "wahab"
+//     console.log(this.username);
+    
+// }
+// chai()   // Output : Undefined 
+
+
+
+// const chai = function (){
+//     let username = "wahab"
+//     console.log(this.username);
+// }
+
+// chai()   // this also undefined in terminal 
+
+
+//+++++++++++++++ Arrow Fun()  +++++++++
+
+// const chai = () => {
+//     let username = "wahab"
+//     console.log(this.username);
+// }
+
+// chai()  // undefined
+
+
+// const chai = () => {
+//     let username = "wahab"
+//     console.log(this);
+// }
+
+// chai()  // output : {}
+
+
+
+
+// core arrow function 
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+
+// console.log(addTwo(30 , 5))   // terminal : 35
+
+
+//Implicit return
+// const addTwo = (num1, num2) => num1 + num2     // no return keyword 
+// console.log(addTwo(30 , 5))  // terminal : 35 
+
+
+// const addTwo = (num1, num2) => (num1 + num2)
+// console.log(addTwo(30 , 5)) // terminal : 35
+
+
+
+//Explicit return
+
+const addTwo = (num1, num2) => ({username: "abdul"})
+console.log(addTwo(30 , 5))  //Terminal : {username : `abdul`}
 
 
 
