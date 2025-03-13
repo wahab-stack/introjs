1b6074e16a80ecd82ed5a1ceccfaabaee18ce59a
Abdul Wahab <wahab.suit997@gmail.com>
2025-03-13 08:14:44 +0000

    while and do while loop discussion

---
+++ 05_Loops/02_whileLoop.js
@@ -0,0 +1,65 @@
+//While loop discussion
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+//Terminal 
+/*
+
+Value of index is 0
+Value of index is 2
+Value of index is 4
+Value of index is 6
+Value of index is 8
+Value of index is 10
+
+*/
+
+// let myArray = ['flash' , 'antman' , 'superman']
+// let arr = 0
+
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+//terminal 
+/*
+
+Value is flash
+Value is antman
+Value is superman
+
+*/
+
+//Do while loop
+
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score <= 10);
+
+//terminal 
+/*
+
+Score is 1
+Score is 2
+Score is 3
+Score is 4
+Score is 5
+Score is 6
+Score is 7
+Score is 8
+Score is 9
+Score is 10
+
+*/
+
+
+
